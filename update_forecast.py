247222e12523e479504f7666eaa7f1b4ed28eb5d
TristanRivaldi <rivaldi.tristan@orange.fr>
2023-10-28 17:34:21 +0200

    modif

--- update_forecast.py
+++ update_forecast.py
@@ -4,8 +4,6 @@ import json
 import pandas as pd
 import subprocess
 
-
-
 # Configurez Git avec les informations d'identification
 git_email = "rivaldi.tristan@orange.fr"
 git_name = "TristanRivaldi"
@@ -14,7 +12,8 @@ os.system(f"git config --global user.email '{git_email}'")
 os.system(f"git config --global user.name '{git_name}'")
 
 # L'URL de l'API
-url = "https://api.open-meteo.com/v1/meteofrance?latitude=43.6109&longitude=3.8763&hourly=temperature_2m,precipitation,windspeed_10m"
+url = 
+"https://api.open-meteo.com/v1/meteofrance?latitude=43.6109&longitude=3.8763&hourly=temperature_2m,precipitation,windspeed_10m"
 
 # Faites une requête vers l'API
 response = requests.get(url)
@@ -33,21 +32,29 @@ if response.status_code == 200:
     # Extrayez la date (jour) à partir de la colonne 'time'
     df['date'] = df['time'].dt.date
 
-    # Agrégation des données par jour
-    daily_data = df.groupby('date').agg({
-        'temperature_2m': ['max', 'min'],
-        'windspeed_10m': 'mean',
-        'precipitation': 'sum'
-    })
+    # Organiser les données en un format adapté pour le tableau HTML
+    daily_data = {}
+    for index, row in df.iterrows():
+        date = row['date']
+        time = row['time'].strftime('%H:%M')
+        temperature = f"{row['temperature_2m']} °C"
+        windspeed = f"{row['windspeed_10m']} km/h"
+        precipitation = f"{row['precipitation']} mm"
 
-    # Renommez les colonnes pour plus de clarté
-    daily_data.columns = ['Température Maximale (°C)', 'Température Minimale (°C)', 'Moyenne du Vent (km/h)', 'Précipitations (mm)']
+        if date not in daily_data:
+            daily_data[date] = []
 
-    # Convertissez les horodatages Unix en millisecondes en dates (jours)
-    daily_data.index = daily_data.index.map(lambda x: x.strftime('%Y-%m-%d'))
+        daily_data[date].append({
+            'time': time,
+            'temperature': temperature,
+            'windspeed': windspeed,
+            'precipitation': precipitation
+        })
 
-    # Enregistrez les données agrégées dans un fichier JSON
-    daily_data.to_json('weather_data.json', orient='index')
+    # Enregistrez les données dans weather_data.json
+    with open('weather_data.json', 'w') as json_file:
+        json.dump(daily_data, json_file)
+
     # Utilisez Git pour ajouter, confirmer et pousser les modifications
     os.system("git add weather_data.json")
     os.system("git commit -m 'Mise à jour des données météorologiques'")
